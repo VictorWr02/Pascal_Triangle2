1e94fa21d453e21805167914fae37ccf54a37f89
Neagu Victor <victorwr2023@gmail.com>
2024-11-15 20:31:23 +0200

    Create Application; Add Button Add SearchBar Push Application

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit
+import sys
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle("Pascal_Triangle")
+    window.setGeometry(100, 100, 600, 400)
+
+    # Adaugam un buton
+    button = QPushButton(window)
+    button.setText("GENERATE")
+    button.move(250, 180)
+
+    # Adăugăm o bară de căutare (QLineEdit)
+    search_bar = QLineEdit(window)
+    search_bar.setPlaceholderText("Search...")
+    search_bar.setGeometry(200, 100, 200, 30)
+
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    application()
+
+
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
